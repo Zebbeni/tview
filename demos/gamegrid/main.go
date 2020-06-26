89f5f01213af1970fde13d308cb855e8ffe4704e
Zebbeni <andrew.albers@gmail.com>
2020-06-25 23:00:13 -0600

    add main menu

---
+++ demos/gamegrid/main.go
@@ -0,0 +1,51 @@
+// Demo code for the Grid primitive.
+package main
+
+import (
+	"github.com/Zebbeni/tview"
+)
+
+var (
+	attributes = map[string]string{
+		"Name": "Casey Jones",
+		"Age":  "24",
+		"Sex":  "M",
+		"Biog": "New York City, where Jones is from, was overrun by criminals. Enticed by examples from TV, Casey decided to do something about the crime in the streets. After equipping himself with a hockey mask and various sports clubs, he began his vigilante campaign.",
+	}
+)
+
+func main() {
+	grid := createGameGrid()
+	if err := tview.NewApplication().SetRoot(grid, true).Run(); err != nil {
+		panic(err)
+	}
+}
+
+func createGameGrid() *tview.Grid {
+	maxAttributeWidth := 0
+	for attribute := range attributes {
+		if len(attribute) > maxAttributeWidth {
+			maxAttributeWidth = len(attribute)
+		}
+	}
+
+	grid := tview.NewGrid()
+	grid.SetTitle("Info")
+	grid.SetColumns(maxAttributeWidth+2, 50)
+
+	row := 0
+	for attribute, value := range attributes {
+		attributeItem := tview.NewTextView().
+			SetTextAlign(tview.AlignCenter).
+			SetText(attribute + ":")
+		valueItem := tview.NewTextView().
+			SetTextAlign(tview.AlignCenter).
+			SetText(value).
+			SetTextAlign(tview.AlignLeft).
+			SetWordWrap(true)
+		grid.AddItem(attributeItem, row, 0, 1, 1, 0, 0, false).
+			AddItem(valueItem, row, 1, 1, 1, 0, 0, false)
+		row++
+	}
+	return grid
+}
